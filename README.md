e2dcaf93cf1d6abc9bd15918a887592202786bc0
FAKEWOL <fakewol@users.noreply.github.com>
2026-04-22 10:33:55 +0300

    chore: AI optimization & README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Weather Condition Checker
+
+## Description
+This repository contains a simple PHP script that determines the weather condition based on temperature.
+
+## Features
+- Determines weather conditions (cok soguk, soguk, serin, sicak, cok sıcak) based on the given temperature.
+- User-friendly HTML interface to display the weather condition.
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/yourusername/S-cakl-k-php-if-yap-s-.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd S-cakl-k-php-if-yap-s-
+   ```
+
+## Usage
+1. Open the `index.php` file in your web browser.
+2. The script will display the weather condition based on the predefined temperature.
+
+## Tech Stack
+- PHP
+- HTML
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- index.php
+++ index.php
@@ -1,29 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-         $sıcaklık =20;
-         if($sıcaklık <= -30 and $sıcaklık >= -10 )
-         $durum = "cok soguk";
-
-         elseif($sıcaklık >= -10 and $sıcaklık <= 0 )
-         $durum = "soguk";
-
-         elseif($sıcaklık >= 1 and $sıcaklık <= 10 )
-         $durum = "serin";
-
-         elseif($sıcaklık >= 11 and $sıcaklık <=30 )
-         $durum = "sıcak";
-
-         else
-         $durum = "cok sıcak";
-         echo $durum;
-    ?>
-</body>
-</html>
+<?php
+	sıcaklık = 20;
+	if ($sıcaklık <= -30 && $sıcaklık >= -10) {
+		$durum = "cok soguk";
+	} elseif ($sıcaklık >= -9 && $sıcaklık <= 0) {
+		$durum = "soguk";
+	} elseif ($sıcaklık >= 1 && $sıcaklık <= 10) {
+		$durum = "serin";
+	} elseif ($sıcaklık >= 11 && $sıcaklık <= 30) {
+		$durum = "sıcak";
+	} else {
+		$durum = "cok sıcak";
+	}
+	echo $durum;
+?>
